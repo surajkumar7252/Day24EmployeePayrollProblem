13ad8bb76768f3743389e8e7d3bf2f6b9be678b2
Surajmal Kumar <surajkumar7252@gmail.com>
2020-10-28 21:53:41 +0530

    [Surajmal Kumar].Adding UC4.Retrieving all employee payroll data added to payroll service DB.

--- payrollservice.sql
+++ payrollservice.sql
@@ -19,6 +19,11 @@ insert into employee_payroll(NAME,SALARY,START) values ('Surajmal Kumar',50000.0
                                                         ('Prakash',85000.00,'2016-06-14'),
                                                         ('Raushan',91000.00,'2018-02-23');
 
+#UseCase 4 : Retrieving all employee payroll data added to payroll service DB.
+
+select * from employee_payroll;
+
+
 
 
 
