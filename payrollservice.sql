630bd81442dd28dd3a9d5994c5df57f323e805b8
Surajmal Kumar <surajkumar7252@gmail.com>
2020-10-28 22:16:19 +0530

    [Surajmal Kumar].Adding UC6.Adding GENDER column to the table and updating the rows to reflect the correct Employee Gender.

--- payrollservice.sql
+++ payrollservice.sql
@@ -28,6 +28,13 @@ select * from employee_payroll;
 select SALARY from employee_payroll where START between cast('2016-06-10' as date) and date(now());
 select SALARY from employee_payroll where START between cast('2016-06-10' as date) and  cast('2018-11-11' as date);
 
+#UseCase 6 : Adding GENDER column to the table and updating the rows to reflect the correct Employee Gender.
+
+alter table employee_payroll add GENDER char(1) after NAME;
+update employee_payroll set GENDER = 'M' where ID =1;
+update employee_payroll set GENDER = 'M' where NAME ='Raushan';
+update employee_payroll set GENDER = 'M' where NAME ='Prakash';
+
 
 
 
