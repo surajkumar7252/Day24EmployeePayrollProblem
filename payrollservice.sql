61676db7bc82e3c380d9b3d0af3e2a2e3e06d5de
Surajmal Kumar <surajkumar7252@gmail.com>
2020-10-28 21:49:13 +0530

    [Surajmal Kumar].Adding UC2.Creating a employee Payroll Table .

--- payrollservice.sql
+++ payrollservice.sql
@@ -4,4 +4,18 @@ create database payroll_service;
 show databases;
 use payroll_service;
 
+#UseCase 2 : Creating an employee payroll Table.
+
+create table employee_payroll(
+ID  int unsigned  Not Null auto_increment,
+NAME  varchar(250) Not Null,
+SALARY double Not Null,
+START date Not Null,
+primary key(ID)); 
+
+
+
+
+
+
 
